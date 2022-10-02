9463751c921fdfa482157596ee19dee4c0f7bc0e
redundant4u <physiogel@pusan.ac.kr>
2022-10-02 10:29:12 +0900

    feat: 터미널 키보드 방향에 따른 기능 추가

--- frontend/lib/src/components/WebTerminal/WebTerminal.tsx
+++ frontend/lib/src/components/WebTerminal/WebTerminal.tsx
@@ -7,12 +7,15 @@ export default class Terminal {
     _options: ITerminalOptions = {
         cursorBlink: true,
         scrollSensitivity: 2,
+        allowProposedApi: true,
     };
     _fitAddon: FitAddon;
+    _input: number;
 
     constructor() {
         this.term = new xterm(this._options);
         this._fitAddon = new FitAddon();
+        this._input = 0;
 
         this._initWebTerminal();
     }
@@ -26,27 +29,55 @@ export default class Terminal {
     }
 
     _initWebTerminal() {
-        this.term.onData((e) => this._onDataHandler(e));
         this.term.onKey((e) => this._onKeyHandler(e));
-
         this.term.loadAddon(this._fitAddon);
+
+        this.term.write('$ ');
     }
 
-    _onDataHandler(e: string) {
-        this.term.write(e);
-    }
+    _onKeyHandler(e: { key: string; domEvent: KeyboardEvent }) {
+        const printable: boolean = !e.domEvent.altKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey;
 
-    _onKeyHandler(e: { domEvent: KeyboardEvent }) {
-        const key = e.domEvent.key;
-
-        console.log(key);
-
-        if (key === 'Enter') {
+        if (e.domEvent.key === 'Enter') {
             this._enter();
+        } else if (e.domEvent.key === 'Backspace') {
+            this._backSpace();
+        } else if (e.domEvent.key === 'ArrowRight') {
+            this._moveRight();
+        } else if (e.domEvent.key === 'ArrowLeft') {
+            this._moveLeft();
+        } else if (printable) {
+            this.term.write(e.key);
+            this._input++;
         }
     }
 
     _enter() {
-        this.term.writeln('');
+        this.term.write('\r\n$ ');
+        this._input = 0;
+    }
+
+    _backSpace() {
+        if (this.term.buffer.active.cursorX > 2) {
+            this.term.write('\b \b');
+            this._input--;
+        }
+    }
+
+    _moveRight() {
+        console.log(this.term.buffer.active.cursorX - 2);
+        console.log(this._input);
+
+        const isEnd: boolean = this.term.buffer.active.cursorX - 2 <= this._input;
+        if (!isEnd) {
+            this.term.write('\x1B[C');
+        }
+    }
+
+    _moveLeft() {
+        const isStart: boolean = this.term.buffer.active.cursorX == 2;
+        if (!isStart) {
+            this.term.write('\x1B[D');
+        }
     }
 }
