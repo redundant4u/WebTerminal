c76af6f16a122054996cf039334e95f2f61ee5e6
redundant4u <physiogel@pusan.ac.kr>
2022-10-09 22:18:28 +0900

    fix: spawn 에러 해결

--- backend/src/Pty.ts
+++ backend/src/Pty.ts
@@ -1,33 +1,33 @@
 import os from 'os';
-import pty from 'node-pty';
+import { spawn, IPty } from 'node-pty';
 import { Socket as SocketIO } from 'socket.io';
 
 class Pty {
     socket: SocketIO;
-    pty: pty.IPty;
+    ptyProcess: IPty;
     shell: string;
 
     constructor(socket: SocketIO) {
         this.socket = socket;
         this.shell = os.platform() === 'win32' ? 'cmd.exe' : 'bash';
 
-        this.initPty();
+        this.init();
     }
 
-    initPty() {
-        this.pty = pty.spawn(this.shell, [], {
+    init() {
+        this.ptyProcess = spawn(this.shell, [], {
             name: 'xterm-color',
             cwd: process.env.HOME,
-            // env: process.env,
+            env: process.env as { [key: string]: string },
         });
 
-        this.pty.onData((data) => {
+        this.ptyProcess.onData((data) => {
             this.send(data);
         });
     }
 
     write(data: string) {
-        this.pty.write(data);
+        this.ptyProcess.write(data);
     }
 
     send(data: string) {
